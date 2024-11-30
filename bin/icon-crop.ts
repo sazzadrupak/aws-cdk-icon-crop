c41a91c94af036723be793f055656c3b7cb4f84b
sazzadrupak <sazzad@oployeelabs.com>
2024-11-30 15:45:33 +0200

    Add CDK

--- bin/icon-crop.ts
+++ bin/icon-crop.ts
@@ -6,7 +6,12 @@ import { IconCropPipelineStack } from '../lib/pipeline';
 
 const app = new cdk.App();
 const stack = new IconCropStack(app, 'IconCropStack', {});
-const pipeline = new IconCropPipelineStack(app, 'IconCropPipelineStack', {});
+const pipeline = new IconCropPipelineStack(app, 'IconCropPipelineStack', {
+  env: {
+    account: '905418160545',
+    region: 'us-east-1',
+  },
+});
 
 // Tags on whole stack
 cdk.Tags.of(stack).add('environment', 'dev');
--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,10 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
-import {
-  CodePipeline,
-  CodePipelineSource,
-  ManualApprovalStep,
-  ShellStep,
-} from 'aws-cdk-lib/pipelines';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+
 import { Construct } from 'constructs';
 import { IconCropStack } from './icon-crop-stack';
 
@@ -12,19 +9,44 @@ export class IconCropPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    // const pipeline = new CodePipeline(this, 'Pipeline', {
+    //   pipelineName: 'IconCropPipeline',
+    //   synth: new ShellStep('Synth', {
+    //     input: CodePipelineSource.gitHub(
+    //       'sazzadrupak/aws-cdk-icon-crop.git',
+    //       'master',
+    //       {
+    //         authentication: cdk.SecretValue.secretsManager('github-token'),
+    //       }
+    //     ),
+    //     commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+    //     primaryOutputDirectory: 'cdk.out',
+    //   }),
+    // });
+    // const devStage = pipeline.addStage(new IconCropAppStage(this, 'dev'));
+    // devStage.addPost(new ManualApprovalStep('approval'));
+
+    const repo = new codecommit.Repository(this, 'IconCropRepo', {
+      repositoryName: 'IconCropRepo',
+    });
+
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
       pipelineName: 'IconCropPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub(
-          'sazzadrupak/aws-cdk-icon-crop.git',
-          'master'
-        ),
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.codeCommit(repo, 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
         primaryOutputDirectory: 'cdk.out',
       }),
     });
-    const devStage = pipeline.addStage(new IconCropAppStage(this, 'dev'));
-    devStage.addPost(new ManualApprovalStep('approval'));
+
+    pipeline.addStage(new IconCropAppStage(this, 'Dev'));
+
+    // Output the CodeCommit repository URL
+    new cdk.CfnOutput(this, 'RepoUrl', {
+      value: repo.repositoryCloneUrlHttp,
+      description: 'The URL of the newly created CodeCommit repository',
+      exportName: 'IconCropRepoUrl',
+    });
   }
 }
 
